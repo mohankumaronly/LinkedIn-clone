b3acc5bab9b8ebd71bd64cae63562ddc59cc819a
mohankumaronly <mohankumaronly81@gmail.com>
2025-12-08 19:13:12 +0530

    files are initialized frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex items-center justify-center flex-col'>
+      <h1 className='text-6xl'>LinkedIn Clone</h1>
+      <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, rem.</p>
+    </div>
+  )
+}
+
+export default App
